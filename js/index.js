7202076560684f6151a4425583fabbd98355ad17
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-07-08 07:34:04 +0330

    [Vendor]- Add Aos Library

--- js/index.js
+++ js/index.js
@@ -1,30 +1,28 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    item:4,
-    center:true,
-    // rewind: true,
-    startPosition:0,
-    autoplay:true,
-    autoplayTimeout:2500,
-    responsive: {
-        0: {
-          items: 1,
-          // nav:true
-        },
-        600: {
-          items: 3,
-          // nav:false
-        },
-        1920: {
-          items: 2,
-          nav: false,
-          loop: true,
-        },
-      },
-})
-
-
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  item: 4,
+  center: true,
+  // rewind: true,
+  startPosition: 0,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  responsive: {
+    0: {
+      items: 1,
+      // nav:true
+    },
+    600: {
+      items: 3,
+      // nav:false
+    },
+    1920: {
+      items: 2,
+      nav: false,
+      loop: true,
+    },
+  },
+});
 
 // ! Tabs Content Logic
 function openCity(evt, cityName) {
@@ -41,9 +39,25 @@ function openCity(evt, cityName) {
   evt.currentTarget.className = "tab-active tablinks ";
 }
 
+// ! Aos Library Init
+AOS.init();
 
-
-
-
-
-
+// ! Type Writer Logic
+let i = 0;
+let txt = "عضو خبرنامه ما شوید";
+let speed = 100;
+let delay = 2000;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type-writer").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(() => {
+      i = 0;
+      document.getElementById("type-writer").innerHTML = "";
+      typeWriter();
+    }, delay);
+  }
+}
+typeWriter();
