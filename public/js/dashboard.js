9c3e1360e851d917c85add80ed553b806c9b3c65
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-07-30 08:18:37 +0330

    [HTML-CSS]- Add Chart Js And Progress bar

---
+++ public/js/dashboard.js
@@ -0,0 +1,89 @@
+// ! Chart Js Config
+
+const saleChart = document.getElementById("myChart1");
+
+new Chart(saleChart, {
+  type: "bar",
+  data: {
+    labels: [
+      "فروردین",
+      "اردیبهشت",
+      "خرداد",
+      "تیر",
+      "مرداد",
+      "شهریور",
+      "مهر ",
+      "آبان",
+      "آذر",
+      "دی",
+      "بهمن",
+      "اسفند",
+    ],
+    datasets: [
+      {
+        label: "میزان فروش",
+        data: [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+const customerChart = document.getElementById("myChart2");
+
+new Chart(customerChart, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "میزان مشتریان",
+        data: [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+const bestSellChart = document.getElementById("myChart3");
+
+new Chart(bestSellChart, {
+  type: "pie",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [1, 2, 3, 4, 5, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
